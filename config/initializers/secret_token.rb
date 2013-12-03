ef444b5b1a1f967f869bba3389393528775215ef
dickeyxxx <jeff@dickey.xxx>
2013-12-03 14:21:15 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LlamaKisses::Application.config.secret_key_base = 'e71174fbb346c2d7ae1552ad1cb1190b4babf62f9a509308a5f6f7724aac6d438554bb434520ddc7e6f781582d92b0900d8dd724c443a4fbd69a3956ae0463ea'
